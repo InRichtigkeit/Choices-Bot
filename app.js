212d8d70ce1201f2cdd6c245fee8f0c0c18891b1
JDihlmann <jdihlmann@de.ibm.com>
2016-01-26 22:26:30 +0100

    Ne Files

---
+++ app.js
@@ -0,0 +1,9 @@
+var Slackbot = require('slackbot')
+
+var slackbot = new Slackbot('inrichtigkeit', 'xoxb-19505835703-QZaaLysknNvRKQpLLiP7DCLC');
+
+slackbot.send("#general", "hello!!", function(err, res, body) {
+	console.log(body);
+  	if(err) return;
+  	console.log(body);
+});
